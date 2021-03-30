3e59d53934daf2e1ca39d53a14fbab4b71d11262
robzvo <robzvo@users.noreply.github.com>
2021-03-29 22:38:08 -0400

    End of Section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,8 @@ namespace API.Entities
     {
         public int ID { get; set; }
         public string UserName { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
